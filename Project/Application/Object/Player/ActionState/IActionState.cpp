13151f3d1704f279e8d4bc458fe6c808877db0c9
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-04-03 17:22:24 +0900

    コメントアウト追加

--- Project/Application/Object/Player/ActionState/IActionState.cpp
+++ Project/Application/Object/Player/ActionState/IActionState.cpp
@@ -9,7 +9,6 @@ void IActionState::PreInitialize(Player* player)
 
 void IActionState::Update()
 {
-
+	// 座標更新
 	player_->worldtransform_.transform_.translate += player_->velocity_ * kDeltaTime_ * (1.0f / IObject::sPlaySpeed);
-
 }
