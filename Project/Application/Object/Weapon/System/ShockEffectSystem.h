a2b6fbaa5c41572c37057b65d012203c69b3a58b
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-05-15 16:41:23 +0900

    コメントアウト

--- Project/Application/Object/Weapon/System/ShockEffectSystem.h
+++ Project/Application/Object/Weapon/System/ShockEffectSystem.h
@@ -5,10 +5,27 @@
 class ShockEffectSystem 
 {
 public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="weapon"></param>
 	void Initialize(Weapon* weapon);
+	/// <summary>
+	/// 更新
+	/// </summary>
 	void Update();
+	/// <summary>
+	/// 衝撃波マネージャー取得
+	/// </summary>
+	/// <returns></returns>
 	ShockWaveManager* GetShockWaveManager() { return shockWaveManager_.get(); }
-	void SetScreenPosition(const Vector2& position) { screenPositionRatio_ = position; }
+	/// <summary>
+	/// スクリーン内のどの位置かを設定
+	/// </summary>
+	/// <param name="position"></param>
+	void SetScreenPosition(const Vector2& position,const BaseCamera& camera) {
+		screenPositionRatio_ = position;
+	}
 private:
 	Weapon* weapon_ = nullptr;
 	Vector2 screenPositionRatio_ = {};
