e2ac95aa9dad7fe0350680f47c2deb44c0eae176
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2024-05-21 02:27:14 +0900

    ファイルディレクトリに投げUI画像とボタンUI画像を追加

---
+++ Project/Application/UI/GameUIManager.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <memory>
+#include <cstdint>
+#include "../../Engine/2D/Sprite.h"
+
+/// <summary>
+/// ゲーム内UIマネージャー
+/// </summary>
+class GameUIManager
+{
+public: // メンバ関数
+
+	/// <summary>
+	/// 初期化関数
+	/// </summary>
+	void Initialze();
+
+	/// <summary>
+	/// 更新関数
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画関数
+	/// </summary>
+	void Draw();
+
+private: // メンバ変数
+
+	
+
+};
+
