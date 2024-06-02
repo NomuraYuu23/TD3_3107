1d959dba5e4a8a4a394a3143a1cf46818422cf06
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-06-02 13:59:41 +0900

    デバッグ用のキー隠した

--- Project/Application/Object/Player/Player.cpp
+++ Project/Application/Object/Player/Player.cpp
@@ -89,14 +89,14 @@ void Player::Update()
 
 //#ifdef _DEBUG
 
-	if (Input::GetInstance()->TriggerKey(DIK_O)) {
-		worldtransform_.transform_.translate = { 300.0f,36.0f };
-		worldtransform_.UpdateMatrix();
-	}
-	if (Input::GetInstance()->TriggerKey(DIK_I)) {
-		worldtransform_.transform_.translate = { 400.0f,50.0f };
-		worldtransform_.UpdateMatrix();
-	}
+	//if (Input::GetInstance()->TriggerKey(DIK_O)) {
+	//	worldtransform_.transform_.translate = { 300.0f,36.0f };
+	//	worldtransform_.UpdateMatrix();
+	//}
+	//if (Input::GetInstance()->TriggerKey(DIK_I)) {
+	//	worldtransform_.transform_.translate = { 400.0f,50.0f };
+	//	worldtransform_.UpdateMatrix();
+	//}
 
 //#endif // _DEBUG
 
