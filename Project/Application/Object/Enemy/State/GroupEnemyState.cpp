af7693e192f6b4515514b4476ace3ff3af1bd95f
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-06-02 13:43:54 +0900

    バグの原因をコメントアウト

--- Project/Application/Object/Enemy/State/GroupEnemyState.cpp
+++ Project/Application/Object/Enemy/State/GroupEnemyState.cpp
@@ -13,12 +13,12 @@ void GroupEnemyState::Initialize()
 void GroupEnemyState::Update()
 {
 
-	if (enemy_->GetMoveStop()) {
-		enemy_->ReleaseParent();
-	}
-	else {
-		enemy_->ResetParent();
-	}
+	//if (enemy_->GetMoveStop()) {
+	//	enemy_->ReleaseParent();
+	//}
+	//else {
+	//	enemy_->ResetParent();
+	//}
 
 	// 行動の更新
 	(this->*actionFuncs_[actionPattern_].updateFunc_)();
